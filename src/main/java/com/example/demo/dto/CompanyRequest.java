0f7739464a59a6e774e74f9629b1a58685bbac1b
SULE <sule1@oocl.com>
2025-09-11 19:55:55 +0800

    refactor(Company): add CompanyRequest and CompanyResponse

---
+++ src/main/java/com/example/demo/dto/CompanyRequest.java
@@ -0,0 +1,19 @@
+package com.example.demo.dto;
+
+public class CompanyRequest {
+    private String name;
+
+    public CompanyRequest() {}
+
+    public CompanyRequest(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
